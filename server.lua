af1d4e701eec0658c1ed1f44c7b99dcb0da37492
José Pedro Dias <jose.pedro.dias@gmail.com>
2020-08-09 02:02:35 +0100

    a bit defensive code

--- server.lua
+++ server.lua
@@ -156,52 +156,56 @@ local function update(t)
         for pIdx = 1, #players do
             local player = players[pIdx]
 
-            if player.dPos[1] ~= 0 or player.dPos[2] ~= 0 then
-                local oldPos = { player.pos[1], player.pos[2] }
+            if player then
+                if player.dPos[1] ~= 0 or player.dPos[2] ~= 0 then
+                    local oldPos = { player.pos[1], player.pos[2] }
 
-                player.pos[1] = player.pos[1] + player.dPos[1]
-                player.pos[2] = player.pos[2] + player.dPos[2]
+                    player.pos[1] = player.pos[1] + player.dPos[1]
+                    player.pos[2] = player.pos[2] + player.dPos[2]
 
-                local ok, isValid = pcall(flood.isHoleValid, player.pos, gc.materials.player[pIdx], mm)
-                if not ok or (ok and not isValid) then
-                    player.pos = oldPos
-                else
-                    local leftMat = gc.materials.dirt
-                    if player.digging then
-                        leftMat = gc.materials.earth
-                    end
+                    local ok, isValid = pcall(flood.isHoleValid, player.pos, gc.materials.player[pIdx], mm)
+                    if not ok or (ok and not isValid) then
+                        player.pos = oldPos
+                    else
+                        local leftMat = gc.materials.dirt
+                        if player.digging then
+                            leftMat = gc.materials.earth
+                        end
 
-                    local deltaDirt = 0
-                    local aux = gc.ahead[ player.dPos[1] .. ',' .. player.dPos[2] ]
-                    for _, p in ipairs(aux) do
-                        local x = player.pos[1] + p[1]
-                        local y = player.pos[2] + p[2]
-                        local v = mm.g(x, y)
-                        if player.digging and v == gc.materials.dirt then
-                            deltaDirt = deltaDirt - 1
-                        elseif not player.digging and v == gc.materials.earth then
-                            deltaDirt = deltaDirt + 1
+                        local deltaDirt = 0
+                        local aux = gc.ahead[ player.dPos[1] .. ',' .. player.dPos[2] ]
+                        for _, p in ipairs(aux) do
+                            local x = player.pos[1] + p[1]
+                            local y = player.pos[2] + p[2]
+                            local v = mm.g(x, y)
+                            if player.digging and v == gc.materials.dirt then
+                                deltaDirt = deltaDirt - 1
+                            elseif not player.digging and v == gc.materials.earth then
+                                deltaDirt = deltaDirt + 1
+                            end
+                        end
+
+                        local skipCarve = false
+                        local holesLeft = player.holesLeft + deltaDirt
+                        if holesLeft < 0 then
+                            player.pos = oldPos
+                            player.digging = false
+                            srv.broadcast('di ' .. pIdx .. ',f')
+                            skipCarve = true
+                        elseif deltaDirt == 0 then
+                        else
+                            player.holesLeft = holesLeft
+                            srv.broadcast('hl ' .. pIdx .. ',' .. player.holesLeft)
+                        end
+
+                        if not skipCarve then
+                            flood.carveHole(oldPos,     leftMat,                   mm)
+                            flood.carveHole(player.pos, gc.materials.player[pIdx], mm)
                         end
                     end
-
-                    local skipCarve = false
-                    local holesLeft = player.holesLeft + deltaDirt
-                    if holesLeft < 0 then
-                        player.pos = oldPos
-                        player.digging = false
-                        srv.broadcast('di ' .. pIdx .. ',f')
-                        skipCarve = true
-                    elseif deltaDirt == 0 then
-                    else
-                        player.holesLeft = holesLeft
-                        srv.broadcast('hl ' .. pIdx .. ',' .. player.holesLeft)
-                    end
-
-                    if not skipCarve then
-                        flood.carveHole(oldPos,     leftMat,                   mm)
-                        flood.carveHole(player.pos, gc.materials.player[pIdx], mm)
-                    end
                 end
+            else
+                print('player not found in update')
             end
         end
     end
@@ -214,31 +218,35 @@ local function update(t)
         for pIdx = 1, #players do
           local pl = players[pIdx]
 
-          for _, si in ipairs(sinks[pIdx]) do
-            if m[si[1]][si[2]] == gc.materials.oil then
-                mm.s(si[1], si[2], gc.materials.sink[pIdx])
-                
-                -- remove from oil cells so it can be filled again
-                local iToRemove
-                for i, v in ipairs(oilCells) do
-                  if v[1] == si[1] and v[2] == si[2] then
-                    iToRemove = i
-                    break
-                  end
+          if pl then
+            for _, si in ipairs(sinks[pIdx]) do
+                if m[si[1]][si[2]] == gc.materials.oil then
+                    mm.s(si[1], si[2], gc.materials.sink[pIdx])
+                    
+                    -- remove from oil cells so it can be filled again
+                    local iToRemove
+                    for i, v in ipairs(oilCells) do
+                    if v[1] == si[1] and v[2] == si[2] then
+                        iToRemove = i
+                        break
+                    end
+                    end
+                    table.remove(oilCells, iToRemove)
+        
+                    pl.captured = pl.captured + 1
+                    --print('player ' .. pIdx .. ' captured ' .. pl.captured .. ' oil')
+                    srv.broadcast('ca ' .. pIdx .. ',' .. pl.captured)
+        
+                    if pl.captured >= winCapture then
+                    --print('player ' .. pIdx .. ' won!')
+                    srv.broadcast('wo ' .. pIdx)
+                    gameGoingOn = false
+                    --love.event.quit()
+                    end
                 end
-                table.remove(oilCells, iToRemove)
-    
-                pl.captured = pl.captured + 1
-                --print('player ' .. pIdx .. ' captured ' .. pl.captured .. ' oil')
-                srv.broadcast('ca ' .. pIdx .. ',' .. pl.captured)
-    
-                if pl.captured >= winCapture then
-                  --print('player ' .. pIdx .. ' won!')
-                  srv.broadcast('wo ' .. pIdx)
-                  gameGoingOn = false
-                  --love.event.quit()
-                end
-              end
+            end
+          else
+            print('player not found in update2')
           end
         end
     end
@@ -261,7 +269,6 @@ generateServer({
   api = srv,
   port = 52225,
   fps = 60,
-  --fps = 6,
   debug = true,
   onUpdate = function(t)
     if nextBleedT then
@@ -272,15 +279,15 @@ generateServer({
     local numClients = srv.getNumClients()
     print('#clients '.. numClients)
     updateClientIdToPlayerIdx()
-    --if numClients == 1 then
-        newGame()
-    --end
+    newGame()
   end,
   onClientLeft = function(clientId, t)
     local numClients = srv.getNumClients()
     print('#clients '.. numClients)
     updateClientIdToPlayerIdx()
-    newGame()
+    if numClients > 0 then
+        newGame()
+    end
   end,
   onReceive = function(data, clientId, t)
     if not gameGoingOn then
